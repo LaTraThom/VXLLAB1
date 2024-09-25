d1fc5cd4fe5287cfeb8649fc569a2463d087f288
LATRATHOM\TIEN DUY <votienduy987@gmail.com>
2024-09-25 16:36:48 +0700

    Ex7

---
+++ SourceCode/LAB1/Core/Src/Ex7.c
@@ -0,0 +1,23 @@
+/*
+ * Ex7.c
+ *
+ *  Created on: Sep 25, 2024
+ *      Author: TIEN DUY
+ */
+
+#include "main.h"
+
+void clearAllClock() {
+	HAL_GPIO_WritePin(LED_1_GREEN_GPIO_Port, LED_1_GREEN_Pin, SET);
+	HAL_GPIO_WritePin(LED_2_RED_GPIO_Port, LED_2_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_3_YELLOW_GPIO_Port, LED_3_YELLOW_Pin, SET);
+	HAL_GPIO_WritePin(LED_4_GREEN_GPIO_Port, LED_4_GREEN_Pin, SET);
+	HAL_GPIO_WritePin(LED_5_RED_GPIO_Port, LED_5_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_6_YELLOW_GPIO_Port, LED_6_YELLOW_Pin, SET);
+	HAL_GPIO_WritePin(LED_7_GREEN_GPIO_Port, LED_7_GREEN_Pin, SET);
+	HAL_GPIO_WritePin(LED_8_RED_GPIO_Port, LED_8_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_9_YELLOW_GPIO_Port, LED_9_YELLOW_Pin, SET);
+	HAL_GPIO_WritePin(LED_10_GREEN_GPIO_Port, LED_10_GREEN_Pin, SET);
+	HAL_GPIO_WritePin(LED_11_RED_GPIO_Port, LED_11_RED_Pin, SET);
+	HAL_GPIO_WritePin(LED_12_YELLOW_GPIO_Port, LED_12_YELLOW_Pin, SET);
+}
